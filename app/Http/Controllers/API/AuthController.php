1a78e6d896c168afe53dc1c44b45e31b8f799d84
Foyzur Rahman Ridoy <foyzur.r.h@gmail.com>
2025-11-17 12:51:06 +0600

    feat(sanctum):User Authentication Completed

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Carbon\Carbon;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Sanctum\PersonalAccessToken;
+
+class AuthController extends Controller
+{
+    public function signup(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'phone_number' => 'required|string|unique:users',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation Error',
+                'error' => $validator->errors()->all(),
+
+            ], 401);
+        }
+
+        try {
+
+            $user = User::create([
+                'name' => $request->name,
+                'phone_number' => $request->phone_number,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User created successfully',
+            ], 201);
+        } catch (Exception $e) {
+            Log::error("User Created Failed: {$e->getMessage()}");
+            return response()->json([
+                'status' => false,
+                'message' => 'User Created Failed' . $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation Error',
+                'error' => $validator->errors()->all(),
+
+            ], 401);
+        }
+
+        try {
+
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user || !Hash::check($request->password, $user->password)) {
+
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Invalid credentials'
+                ], 401);
+            }
+
+            PersonalAccessToken::where('tokenable_id', $user->id)
+                ->where('tokenable_type', get_class($user))
+                ->whereNotNull('expires_at')
+                ->where('expires_at', '<', Carbon::now())
+                ->delete();
+
+            $token = $user->createToken('auth_token', [], now()->addMinutes(10));
+
+            Log::info("User logged in successfully: {$user->email}");
+
+
+            return response()->json([
+                'status' => true,
+                'access_token' => $token->plainTextToken,
+                'token_type' => 'Bearer',
+                'expires_at' => $token->accessToken->expires_at
+            ]);
+        } catch (Exception $e) {
+            Log::error("User logged Failed: {$e->getMessage()}");
+            return response()->json([
+                'status' => false,
+                'message' => 'User logged Failed' . $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Logged out successfully'
+        ]);
+    }
+}
